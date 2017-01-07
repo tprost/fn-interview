7f2a51441a9bc83ca24152ed8043e284b8f73fdb
tprost <tprost@users.noreply.github.com>
2017-01-07 15:28:57 -0500

    update sequelize config

--- config/db/index.js
+++ config/db/index.js
@@ -1,3 +1,8 @@
 'use strict';
 
+if (!process.env.NODE_ENV) {
+  console.warn("NODE_ENV not set... using NODE_ENV=development");
+  process.env.NODE_ENV = 'development';
+}
+
 module.exports = require('./' + process.env.NODE_ENV + '.js');
